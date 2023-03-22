93c0a4b5b3b306a59737abf6d5aafc912be202f2
Noah Mudd <noah.mudd@outlook.com>
2023-03-22 19:24:58 -0400

    added files

---
+++ WellBeanAPI/Models/UserInputSurvey.cs
@@ -0,0 +1,19 @@
+﻿namespace WellBeanAPI.Models
+{
+    public class UserInputSurvey
+    {
+        public string? UserName { get; set; }
+
+        public int MinutesExercised { get; set; }
+
+        public int OzWater { get; set; }
+
+        public int DietRating { get; set; }
+
+        public int HoursSlept { get; set; }
+
+        public bool DidYouWork { get; set; }
+
+        public int WellbeingRating { get; set; }
+    }
+}
